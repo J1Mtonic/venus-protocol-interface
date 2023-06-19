f62834491eb759b5c3428fe20d4ccd843562b0c5
therealemjy <maxime.julian.creations@gmail.com>
2023-06-19 16:11:37 +0800

    update how token is retrieved for each transaction

--- src/clients/api/queries/getTransactions/formatTransaction.ts
+++ src/clients/api/queries/getTransactions/formatTransaction.ts
@@ -2,9 +2,11 @@ import BigNumber from 'bignumber.js';
 import { TransactionCategory, TransactionEvent } from 'types';
 import { convertTokensToWei, getTokenByAddress, getVTokenByAddress } from 'utilities';
 
-import { TOKENS } from 'constants/tokens';
+import { MAINNET_VBEP_TOKENS, TOKENS } from 'constants/tokens';
 
 import { TransactionResponse } from './types';
+
+const MAIN_POOL_VXVS_ADDRESS = '0x151b1e2635a717bcdc836ecd6fbb62b674fe3e1d';
 
 const formatTransaction = ({
   amount,
@@ -14,17 +16,17 @@ const formatTransaction = ({
   timestamp,
   ...rest
 }: TransactionResponse) => {
-  const token =
-    getTokenByAddress(tokenAddress) ||
-    getVTokenByAddress(tokenAddress)?.underlyingToken ||
-    TOKENS.xvs;
+  const tokenOrVToken =
+    category === 'vtoken'
+      ? getVTokenByAddress(tokenAddress) || MAINNET_VBEP_TOKENS[MAIN_POOL_VXVS_ADDRESS]
+      : getTokenByAddress(tokenAddress) || TOKENS.xvs;
 
   return {
     ...rest,
-    amountWei: convertTokensToWei({ value: new BigNumber(amount), token }),
+    amountWei: convertTokensToWei({ value: new BigNumber(amount), token: tokenOrVToken }),
     category: category as TransactionCategory,
     event: event as TransactionEvent,
-    token,
+    token: 'asset' in tokenOrVToken ? tokenOrVToken : tokenOrVToken.underlyingToken,
     timestamp: new Date(timestamp * 1000), // Convert timestamp to milliseconds
   };
 };
--- src/utilities/convertTokensToWei.ts
+++ src/utilities/convertTokensToWei.ts
@@ -1,7 +1,7 @@
 import BigNumber from 'bignumber.js';
-import { Token } from 'types';
+import { Token, VToken } from 'types';
 
-export const convertTokensToWei = ({ value, token }: { value: BigNumber; token: Token }) =>
+export const convertTokensToWei = ({ value, token }: { value: BigNumber; token: Token | VToken }) =>
   value.multipliedBy(new BigNumber(10).pow(token.decimals)).dp(0);
 
 export default convertTokensToWei;
