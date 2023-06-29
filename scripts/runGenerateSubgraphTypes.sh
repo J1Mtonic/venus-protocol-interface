c73001ed2287b9583af4cd3306a2852b8368fb02
Gleiser Oliveira <gleiser.dev@gmail.com>
2023-06-29 14:28:31 -0600

    fix: subgraph URL in runGenerateSubgraphTypes.sh

--- scripts/runGenerateSubgraphTypes.sh
+++ scripts/runGenerateSubgraphTypes.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-if [[ "${VITE_ENVIRONMENT}" == "preview" || "${VITE_ENVIRONMENT}" == "mainnet" ]]; then
-  export SUBGRAPH_GRAPHQL_ENDPOINT="https://api.thegraph.com/subgraphs/name/venusprotocol/venus-isolated-pools"
+if [[ "${VITE_ENVIRONMENT}" == "testnet" ]]; then
+    export SUBGRAPH_GRAPHQL_ENDPOINT="https://api.thegraph.com/subgraphs/name/venusprotocol/venus-isolated-pools-chapel"
 else
-  export SUBGRAPH_GRAPHQL_ENDPOINT="https://api.thegraph.com/subgraphs/name/venusprotocol/venus-isolated-pools-chapel"
+  export SUBGRAPH_GRAPHQL_ENDPOINT="https://api.thegraph.com/subgraphs/name/venusprotocol/venus-isolated-pools"
 fi
+
+echo '$SUBGRAPH_GRAPHQL_ENDPOINT'$SUBGRAPH_GRAPHQL_ENDPOINT
 
 yarn graphql-code-generator --config ./src/clients/subgraph/codegen.ts
